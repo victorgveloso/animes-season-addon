e41ec2f1e42118bf0b4ed73106fa48aa789679b9
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-06-20 02:29:08 -0400

    Works for clean environment

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
-import { monthToSeason, createSortedSeasonList } from "./query";
 import { Stremio } from "./stremio";
 import { TitleType } from 'name-to-imdb';
 
 async function main() {
     const today = new Date();
     const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth();
-    const currentSeason = monthToSeason(currentMonth);
-    const seasons = createSortedSeasonList(currentSeason);
+    const manifest = await Stremio.createManifestIfNotExists(".");
     const promises = [];
     for (const titleType of ["series", "movie"] as TitleType[]) {
-        for (const season of seasons) {
+        for (const season of manifest.getSeasons()) {
+            console.log(`Generating catalog for season ${season} ${titleType}`);
             const catalog = await Stremio.createCatalogIfNotExists(`${titleType}/latest_anime_seasons/genre=${season.toLowerCase()}.json`);
-            catalog.populate(currentYear, season, titleType);
+            await catalog.populate(currentYear, season, titleType);
             promises.push(catalog.writeToFile());
         }
-        for (let year = 2001; year < currentYear; year++) {
-            const catalog = await Stremio.createCatalogIfNotExists(`${titleType}/archive_anime_seasons/genre=${year}.json`);
-            for (const season of seasons) {
-                catalog.populate(year, season, titleType);
+        if (currentYear > manifest.getLastUpdate().getFullYear()) {
+            for (let year = Math.max(2001, manifest.getLastUpdate().getFullYear()); year < currentYear; year++) {
+                console.log(`Generating catalog for year ${year} ${titleType}`);
+                const catalog = await Stremio.createCatalogIfNotExists(`${titleType}/archive_anime_seasons/genre=${year}.json`);
+                for (const season of manifest.getSeasons()) {
+                    await catalog.populate(year, season, titleType);
+                }
+                promises.push(catalog.writeToFile());
             }
-            promises.push(catalog.writeToFile());
         }
+    }
+    if (currentYear > manifest.getLastUpdate().getFullYear()) {
+        await manifest.update(today);
     }
     await Promise.all(promises);
 }
--- src/stremio.ts
+++ src/stremio.ts
@@ -1,7 +1,7 @@
 import { TitleType } from "name-to-imdb";
 import { promises as fs } from "fs";
 import path from "path";
-import { Season, Sorting, query } from "./query";
+import { Season, Sorting, createSortedSeasonList, monthToSeason, query } from "./query";
 import { Anilist, Imdb } from "./request";
 export type Meta = {
     id: string;
@@ -10,21 +10,18 @@ export type Meta = {
     poster: string;
 }
 export class Catalog {
-    #metas: Meta[] = [];
-    #path: string;
-    constructor(public readonly path: string) {
-        this.#path = path;
-    }
+    private metas: Meta[] = [];
+    constructor(private pathFile: string) {}
         
     addMeta(meta: Meta) {
-        this.#metas.push(meta);
+        this.metas.push(meta);
     }
 
     /**
-     * Create a file at #path with the contents of #metas
+     * Create a file at path with the contents of metas
      */
     async writeToFile() {
-        await fs.writeFile(this.#path, JSON.stringify({metas: this.#metas}, null, 2));
+        await fs.writeFile(this.pathFile, JSON.stringify({metas: this.metas}, null, 2));
     }
 
     async populate(year:number, season:Season, type:TitleType) {
@@ -42,17 +39,193 @@ export class Catalog {
         }
     }
 }
+export type CatalogExtra = {
+    name: string;
+    options: string[];
+    isRequired: boolean;
+}
+export type CatalogDescription = {
+    id: string;
+    type: TitleType;
+    name: string;
+    extra: CatalogExtra[];
+    extraSupported: string[];
+}
+enum CatalogType {
+    LATEST = "latest",
+    ARCHIVE = "archive"
+}
+type AbstractManifest = {
+    id: string;
+    version: string;
+    name: string;
+    description: string;
+    logo: string;
+    resources: string[];
+    types: string[];
+    catalogs: CatalogDescription[];
+    idPrefixes: string[];
+    lastUpdatedAt?: Date;
+}
+export class Manifest implements AbstractManifest {
+    async update(today: Date) {
+        this.lastUpdatedAt = today;
+        this.years = Manifest.dateToYears(today);
+        this.seasons = Manifest.dateToSeasons(today);
+        for (const catalog of this.catalogs) {
+            if (catalog.name == "Anime Years") {
+                catalog.extra[0].options = this.years;
+            }
+        }
+        await this.writeToFile();
+    }
+
+    /**
+     * Create a file at path with the contents of metas
+     */
+    async writeToFile() {
+        await fs.writeFile(this.pathFile, JSON.stringify({
+            id: this.id,
+            version: this.version,
+            name: this.name,
+            description: this.description,
+            logo: this.logo,
+            resources: this.resources,
+            types: this.types,
+            catalogs: this.catalogs,
+            idPrefixes: this.idPrefixes,
+            lastUpdatedAt: this.lastUpdatedAt,
+        }, null, 2));
+    }
+
+    getLastUpdate() : Date {
+        return this.lastUpdatedAt;
+    }
+    private seasons: Season[];
+    private years: string[];
+    constructor(
+        private pathFile: string,
+        public id: string,
+        public version: string,
+        public name: string,
+        public description: string,
+        public logo: string,
+        public resources: string[],
+        public types: string[],
+        public idPrefixes: string[],
+        public catalogs: CatalogDescription[],
+        public lastUpdatedAt: Date = new Date(1970, 0, 1)
+    ){
+        if (catalogs.length == 0) {
+            this.catalogs = [];
+            this.seasons = Manifest.dateToSeasons(lastUpdatedAt);
+            this.years = Manifest.dateToYears(lastUpdatedAt);
+            this.addCatalog("series", CatalogType.LATEST);
+            this.addCatalog("movie", CatalogType.LATEST);
+            this.addCatalog("series", CatalogType.ARCHIVE);
+            this.addCatalog("movie", CatalogType.ARCHIVE);
+        }
+        else {
+            this.catalogs = catalogs;
+            this.seasons = catalogs.find(catalog => catalog.id == "latest_anime_seasons")?.extra[0].options as Season[];
+            this.years = catalogs.find(catalog => catalog.id == "archive_anime_seasons")?.extra[0].options as string[];
+        }
+    }
+    static fromObject(pathFile: string, obj: AbstractManifest) : Manifest {
+        return new Manifest(
+            pathFile,
+            obj.id,
+            obj.version,
+            obj.name,
+            obj.description,
+            obj.logo,
+            obj.resources,
+            obj.types,
+            obj.idPrefixes,
+            obj.catalogs,
+            obj?.lastUpdatedAt
+        );
+    }
+    static default(pathFile: string) : Manifest {
+        return Manifest.fromObject(pathFile, {
+            id: "animes-season-addon",
+            version: "1.1.0",
+            name: "Animes' season",
+            description: "A catalog addon for the latest anime seasons",
+            logo: "https://styles.redditmedia.com/t5_yo4xr/styles/communityIcon_rv2fpnvbmfra1.png",
+            resources: ["catalog"],
+            types: ["movie","series"],
+            catalogs: [],
+            idPrefixes: ["tt"]
+          } as AbstractManifest);
+    }
+    static async fromFile(filePath: string) : Promise<Manifest> {
+        const manifest = await fs.readFile(filePath, "utf-8");
+        return Manifest.fromObject(filePath, JSON.parse(manifest));
+    }
+    getSeasons() : Season[] {
+        return this.seasons;
+    }
+    getYears() : string[] {
+        return this.years;
+    }
+    private static dateToYears(today: Date) : string[] {
+        const years = []
+        for (let y = 2001; y < today.getFullYear(); y++) {
+            years.push(y.toString());
+        }
+        return years
+    }
+    private static dateToSeasons(today: Date) : Season[] {
+        const currentMonth = today.getMonth();
+        const currentSeason = monthToSeason(currentMonth);
+        return createSortedSeasonList(currentSeason);
+    }
+    private addCatalog(contentType: TitleType, catalogType: CatalogType) {
+        this.catalogs.push({
+            type: contentType,
+            id: catalogType == CatalogType.LATEST ? "latest_anime_seasons" : "archive_anime_seasons",
+            name: catalogType == CatalogType.LATEST ? "Anime Seasons" : "Anime Years",
+            extra: [{ 
+                name: "genre", 
+                options: catalogType == CatalogType.LATEST ? this.seasons : this.years, 
+                isRequired: true }
+            ],
+            "extraSupported": ["genre"]
+          });
+    }
+}
+  
+
 
 export class Stremio {
+    private static baseDir = ".";
+    static async createManifestIfNotExists(filePath: string) : Promise<Manifest> {
+        const manifestPath = `${this.baseDir}/${filePath}/manifest.json`;
+        const dir = path.dirname(manifestPath);
+        let manifest: Manifest;
+        try {
+            await fs.stat(dir);
+            await fs.stat(manifestPath);
+            manifest = await Manifest.fromFile(manifestPath);
+            console.log(`Manifest found at ${manifestPath}`);
+            
+        } catch (error) {
+            await fs.mkdir(dir, { recursive: true });
+            manifest = Manifest.default(manifestPath);
+            await manifest.writeToFile();
+            console.log(`Manifest created at ${manifestPath}`); 
+        }
+        return manifest;
+    }
     static async createCatalogIfNotExists(filePath: string) : Promise<Catalog> {
-        const baseDir = "catalog";
-        const dir = path.dirname(`${baseDir}/${filePath}`);
+        const dir = path.dirname(`${this.baseDir}/catalog/${filePath}`);
         try {
             await fs.stat(dir);
         } catch (error) {
             await fs.mkdir(dir, { recursive: true });
         }
-        return new Catalog(`${baseDir}/${filePath}`);
+        return new Catalog(`${this.baseDir}/catalog/${filePath}`);
     }
     static removeSeasonDetails(base: string): string {
         const stopWords = [
