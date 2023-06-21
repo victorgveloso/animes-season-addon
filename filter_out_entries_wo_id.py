eef7edcf9a5dd7eca1fc8a4fb355a001d35fbb6f
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-06-21 07:35:54 -0400

    Prevent utility script from breaking unicode chars

--- filter_out_entries_wo_id.py
+++ filter_out_entries_wo_id.py
@@ -6,4 +6,4 @@ for i in glob.iglob("catalog/*/*/*.json"):
         data = json.load(f)["metas"]
     data = [x for x in data if "id" in x and x["id"].startswith("tt")]
     with open(i, "w") as f:
-        json.dump({"metas": data}, f, indent=2)
+        json.dump({"metas": data}, f, indent=2, ensure_ascii=False)
